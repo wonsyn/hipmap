210115d5b0e6c4f8edd926d77cfc83f006bb9ec3
정진 <supermilktank8@gmail.com>
2022-10-21 01:20:21 +0900

    feat_Jin: 간단한 모달 완성

--- frontend/src/components/modal/Modal.tsx
+++ frontend/src/components/modal/Modal.tsx
@@ -1,3 +1,26 @@
 import React from "react";
+import { createPortal } from "react-dom";
+import { ModalBackDrop, ModalContent } from "../../styles/modal";
 
-const Modal = () => {};
+interface ModalHandlerProps {
+  modalHandler: () => void;
+}
+
+const Modal = ({ modalHandler }: ModalHandlerProps) => {
+  return createPortal(
+    <>
+      <ModalContent
+        width="100px"
+        height="100px"
+        border_radius={null}
+        backgroundcolor={null}
+      >
+        test
+      </ModalContent>
+      <ModalBackDrop onClick={modalHandler} />
+    </>,
+    document.getElementById("modalDiv") as HTMLElement
+  );
+};
+
+export default Modal;
