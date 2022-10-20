79cc7184ec748497e1eae626b475657fedd216ef
wonsyn <sde3715@naver.com>
2022-10-20 11:00:45 +0900

    feat_wonsyn: S07P31B108-42 프로젝트 생성

---
+++ Backend/hipmap/src/main/java/com/hipmap/HipmapApplication.java
@@ -0,0 +1,13 @@
+package com.hipmap;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HipmapApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HipmapApplication.class, args);
+    }
+
+}
