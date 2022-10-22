3891eabf9ab34b504713d72e3bdcdf5cc83feb19
정진 <supermilktank8@gmail.com>
2022-10-22 20:09:21 +0900

    feat_jin: 메인화면 장소 둘러보기

--- frontend/src/pages/main/Main.tsx
+++ frontend/src/pages/main/Main.tsx
@@ -1,13 +1,26 @@
 /** @jsxImportSource @emotion/react */
 
+import { css } from "@emotion/react";
 import BestHipPlace from "./component/bestHipPlace";
 import MyHipContainer from "./component/MyHip";
+import SameHipPlace from "./component/sameHipPlace";
 
 const Main = () => {
   return (
-    <div>
+    <div style={{ marginBottom: "10%" }}>
       <BestHipPlace />
       <MyHipContainer />
+      <div
+        css={css`
+          width: 100%;
+          display: flex;
+          flex-direction: column;
+          justify-content: center;
+          align-items: center;
+        `}
+      >
+        <SameHipPlace />
+      </div>
     </div>
   );
 };
