5bbf3af09541ae6c56cc7a923c886776ad0f5384
정진 <supermilktank8@gmail.com>
2022-10-22 17:39:09 +0900

    feat_jin: 메인 화면에서 myHip 컴포넌트 추가

--- frontend/src/pages/main/Main.tsx
+++ frontend/src/pages/main/Main.tsx
@@ -1,11 +1,13 @@
 /** @jsxImportSource @emotion/react */
 
 import BestHipPlace from "./component/bestHipPlace";
+import MyHipContainer from "./component/MyHip";
 
 const Main = () => {
   return (
     <div>
       <BestHipPlace />
+      <MyHipContainer />
     </div>
   );
 };
