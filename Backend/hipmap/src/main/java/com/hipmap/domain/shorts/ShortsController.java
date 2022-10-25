5e45d8dbe451b8e1c4f1e5a9fd155ae3dcc117c1
DESKTOP-KVCQHCD\SSAFY <jangwonseok17@gmail.com>
2022-10-25 15:00:40 +0900

    feat_Jang: S07P31B108-154 쇼츠 조회 기능 구현 - 랜덤 쇼츠 영상 페에지의 쇼츠 페이지네이션 기능 구현

--- Backend/hipmap/src/main/java/com/hipmap/domain/shorts/ShortsController.java
+++ Backend/hipmap/src/main/java/com/hipmap/domain/shorts/ShortsController.java
@@ -1,4 +1,25 @@
 package com.hipmap.domain.shorts;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Pageable;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/shorts")
 public class ShortsController {
+
+    @Autowired
+    ShortsService shortsService;
+
+    @GetMapping()
+    List<ShortsEntity> getShorts(Pageable pageable) {
+        return shortsService.getShorts(pageable).getContent(); // 페이지 객체 어쩌구 : 필요함
+    }
+
+
+
 }
--- Backend/hipmap/src/main/java/com/hipmap/domain/shorts/ShortsServiceImpl.java
+++ Backend/hipmap/src/main/java/com/hipmap/domain/shorts/ShortsServiceImpl.java
@@ -1,4 +1,22 @@
 package com.hipmap.domain.shorts;
 
-public class ShortsServiceImpl {
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.stereotype.Service;
+
+
+
+@Service
+public class ShortsServiceImpl implements ShortsService{
+
+    @Autowired
+    ShortsRepository shortsRepository;
+
+    @Override
+    public Page<ShortsEntity> getShorts(Pageable pageable) {
+        return shortsRepository.findAll(pageable);
+    }
+
+
 }
