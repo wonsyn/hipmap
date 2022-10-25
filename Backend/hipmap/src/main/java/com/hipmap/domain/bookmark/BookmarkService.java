e293ffab0e5e17745426995f73fcaa33e84a8d02
유영진 <dugajada032@gmail.com>
2022-10-25 12:36:36 +0900

    feat :jpa config 추가

--- Backend/hipmap/src/main/java/com/hipmap/domain/bookmark/BookmarkService.java
+++ Backend/hipmap/src/main/java/com/hipmap/domain/bookmark/BookmarkService.java
@@ -1,7 +1,10 @@
 package com.hipmap.domain.bookmark;
 
+
 public interface BookmarkService {
 
     BookmarkEntity createBookmark(Long userId, Long shortsId);
+
+//    void deleteBookmark(Long userId, Long shortsId);
 
 }
