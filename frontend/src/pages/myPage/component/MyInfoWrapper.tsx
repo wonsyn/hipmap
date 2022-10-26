e4b27cafb97d68cbc7ca73c6f0e351375a24e6f2
정진 <supermilktank8@gmail.com>
2022-10-27 00:51:12 +0900

    feat_jin: 마이페이지 뼈대 잡는 중

--- frontend/src/pages/myPage/component/MyInfoWrapper.tsx
+++ frontend/src/pages/myPage/component/MyInfoWrapper.tsx
@@ -1,8 +1,20 @@
+import { MyInfoDiv, MyInfoWrapperDiv } from "../styles/MyInfoWrapperStyle";
+import { useSelector } from "react-redux";
+import { RootState } from "../../../store/store";
+import AccountCircleIcon from "@mui/icons-material/AccountCircle";
 const MyInfoWrapper = () => {
+  const userInfo = useSelector((store: RootState) => store.userReducer.user);
+  console.log(userInfo);
   return (
-    <div>
-      <div></div>
-    </div>
+    <MyInfoWrapperDiv>
+      <MyInfoDiv>
+        <div>
+          <AccountCircleIcon fontSize="large" />
+        </div>
+        <div>{userInfo.nickname}</div>
+        <div>{userInfo.labeling}</div>
+      </MyInfoDiv>
+    </MyInfoWrapperDiv>
   );
 };
 
