637ce409084523883b7e1a9e1ebeb8f74e851fd5
정진 <supermilktank8@gmail.com>
2022-10-28 17:52:02 +0900

    fix_jin: 페이지 스크롤 작성중

--- frontend/src/pages/shorts/index.tsx
+++ frontend/src/pages/shorts/index.tsx
@@ -1,31 +1,54 @@
 import { useEffect, useRef, useState } from "react";
 import ShortsVideoWrapper from "./component/ShortsVideoWrapper";
-import {
-  ShortsVideoAreaDiv,
-  ShortsVideoWrapperDiv,
-  ShortsWrapperDiv,
-} from "./styles/shortsStyle";
-import { FullPage, Slide } from "react-full-page";
+import { ShortsVideoAreaDiv, ShortsWrapperDiv } from "./styles/shortsStyle";
 
 const Shorts = () => {
   const [shortsData, setShortsData] = useState<number[]>([
     0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10,
   ]);
-  const scrollRef = useRef<HTMLDivElement | null>(null);
+  const scrollRef = useRef<HTMLDivElement>(null);
+  useEffect(() => {
+    console.log(scrollRef);
+    const wheelHandler = (e: WheelEvent) => {
+      e.preventDefault();
+      const { deltaY } = e;
+      const scrollTop = scrollRef.current?.scrollTop;
+      const pageHeight = window.innerHeight;
+      console.log(deltaY, pageHeight);
+      if (deltaY > 0) {
+        if (
+          scrollTop !== undefined &&
+          scrollTop >= 0 &&
+          scrollTop < pageHeight
+        ) {
+          console.log("현재 1페이지");
+          scrollRef.current?.scrollTo({
+            top: pageHeight,
+            left: 0,
+            behavior: "smooth",
+          });
+        }
+      }
+    };
+    const scrollHandler = (e: any) => {
+      e.preventDefault();
+      console.log(window.scrollY);
+    };
+    const scrollRefCurrent = scrollRef.current;
+    scrollRefCurrent?.addEventListener("scroll", scrollHandler);
+    scrollRefCurrent?.addEventListener("wheel", wheelHandler);
+    return () => {
+      scrollRefCurrent?.removeEventListener("wheel", wheelHandler);
+    };
+  }, []);
 
   return (
-    <ShortsWrapperDiv>
-      <ShortsVideoWrapperDiv ref={scrollRef}>
-        <FullPage>
-          {shortsData.map((e, i) => (
-            // <ShortsVideoAreaDiv key={i}>
-            <Slide>
-              <ShortsVideoWrapper data={e} />
-            </Slide>
-            // </ShortsVideoAreaDiv>
-          ))}
-        </FullPage>
-      </ShortsVideoWrapperDiv>
+    <ShortsWrapperDiv ref={scrollRef}>
+      {shortsData.map((e, i) => (
+        <ShortsVideoAreaDiv key={i}>
+          <ShortsVideoWrapper data={e} />
+        </ShortsVideoAreaDiv>
+      ))}
     </ShortsWrapperDiv>
   );
 };
