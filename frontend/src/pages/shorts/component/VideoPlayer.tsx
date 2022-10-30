90276f106bb11062714f3c4c8ccd25041336f884
정진 <supermilktank8@gmail.com>
2022-10-30 23:06:25 +0900

    Feat_Jin: 쇼츠 화면 구현

---
+++ frontend/src/pages/shorts/component/VideoPlayer.tsx
@@ -0,0 +1,25 @@
+import { useRef } from "react";
+import { useIsVideoLoaded } from "../../../hoc/useIsVideoLoaded";
+import {
+  ShortsVideoPlayerVideo,
+  ShortsVideoPlayerWrapperDiv,
+} from "../styles/shortsStyle";
+const ShortsVideoPlayer = ({ file_src }: { file_src: string }) => {
+  const lazy = true;
+  const { videoRef, isLoaded } = useIsVideoLoaded(lazy);
+  const elementRef = useRef<HTMLVideoElement>(null);
+
+  return (
+    <ShortsVideoPlayerWrapperDiv ref={videoRef}>
+      {isLoaded ? (
+        <ShortsVideoPlayerVideo ref={elementRef} muted loop autoPlay controls>
+          <source src={file_src} type="video/mp4"></source>
+        </ShortsVideoPlayerVideo>
+      ) : (
+        <div>isLoading....</div>
+      )}
+    </ShortsVideoPlayerWrapperDiv>
+  );
+};
+
+export default ShortsVideoPlayer;
