3f60e5f25918473d6c09719cb7463f35e0379166
wonsyn <sde3715@naver.com>
2022-10-31 10:29:12 +0900

    Fix merge conflicts

--- Backend/hipmap/src/main/java/com/hipmap/domain/user/UserController.java
+++ Backend/hipmap/src/main/java/com/hipmap/domain/user/UserController.java
@@ -62,20 +62,4 @@ public class UserController {
                 .expireMilliSec(JwtUtil.TOKEN_VALIDATION_SECOND)
                 .build();
     }
-    private final UserService userService;
-    @PostMapping("email")
-    @ApiOperation(value = "이메일 인증", notes = "인증코드를 받을 이메일을 보내고 인증코드를 받는다.")
-        public ResponseEntity<String> authEmail(@RequestBody @Valid EmailRequestDto request) {
-        userService.authEmail(request);
-        return ResponseEntity.ok().build();
-    }
-
-    @GetMapping
-    public void emailConfirm(@RequestParam("key") String key) {
-
-    }
-
-
-
-
 }
