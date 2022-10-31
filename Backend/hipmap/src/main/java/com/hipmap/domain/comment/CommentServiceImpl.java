06eaf30fb70fcd66016e57305fc71f521ae38d16
유영진 <dugajada032@gmail.com>
2022-10-31 12:33:53 +0900

    feat : 알림 이동 url 추가

--- Backend/hipmap/src/main/java/com/hipmap/domain/comment/CommentServiceImpl.java
+++ Backend/hipmap/src/main/java/com/hipmap/domain/comment/CommentServiceImpl.java
@@ -3,6 +3,7 @@ package com.hipmap.domain.comment;
 import com.hipmap.domain.comment.request.CreateCommentRequest;
 import com.hipmap.domain.comment.request.UpdateCommentRequest;
 import com.hipmap.domain.comment.response.GetCommentResponse;
+import com.hipmap.domain.notification.NotificationService;
 import com.hipmap.domain.shorts.ShortsEntity;
 import com.hipmap.domain.shorts.ShortsRepository;
 import com.hipmap.domain.user.UserEntity;
@@ -31,6 +32,9 @@ public class CommentServiceImpl implements CommentService {
     @Autowired
     ShortsRepository shortsRepository;
 
+    @Autowired
+    NotificationService notificationService;
+
     @Override
     @Transactional
     public CommentEntity createComment(Long userId, Long shortsId, CreateCommentRequest request) {
@@ -48,6 +52,7 @@ public class CommentServiceImpl implements CommentService {
                         .sequence(request.getSequence())
                         .createTime(LocalDateTime.now())
                         .build();
+                notificationService.send(user,"댓글이 작성되었습니다");
                 return commentRepository.save(newComment);
             } else throw new IllegalArgumentException("존재하지 않는 쇼츠입니다.");
         } else throw new IllegalArgumentException("존재하지 않는 유저입니다.");
@@ -57,11 +62,11 @@ public class CommentServiceImpl implements CommentService {
     @Transactional
     public CommentEntity updateComment(Long shortsId, Long commentId, UpdateCommentRequest request) {
         Optional<CommentEntity> cmtOp = commentRepository.findById(commentId);
-        if(cmtOp.isPresent()){
+        if (cmtOp.isPresent()) {
             CommentEntity updateCmt = cmtOp.get();
             updateCmt.setContent(request.getContent());
             return updateCmt;
-        }else throw new IllegalArgumentException("존재하지 않는 commentId입니다.");
+        } else throw new IllegalArgumentException("존재하지 않는 commentId입니다.");
     }
 
     @Override
@@ -69,7 +74,7 @@ public class CommentServiceImpl implements CommentService {
         List<CommentEntity> commentEntities = commentReposiotrySupport.findCommentsByShortsId(shortsId);
 
         List<GetCommentResponse> commentList = commentEntities.stream()
-                .map(m-> new GetCommentResponse(m.getCommentId(),m.getContent(),m.getCommentGroup(),m.getSequence(),m.getCreateTime()))
+                .map(m -> new GetCommentResponse(m.getCommentId(), m.getContent(), m.getCommentGroup(), m.getSequence(), m.getCreateTime()))
                 .collect(Collectors.toList());
         return commentList;
     }
@@ -77,10 +82,10 @@ public class CommentServiceImpl implements CommentService {
     @Override
     public void deleteComment(Long userId, Long commentId) {
 
-           Optional<CommentEntity> commentOp = commentRepository.findById(commentId);
-           if(commentOp.isPresent()){
-               commentRepository.deleteById(commentId);
-           }else throw new IllegalArgumentException("존재하지 않는 commentid입니다.");
+        Optional<CommentEntity> commentOp = commentRepository.findById(commentId);
+        if (commentOp.isPresent()) {
+            commentRepository.deleteById(commentId);
+        } else throw new IllegalArgumentException("존재하지 않는 commentid입니다.");
 
     }
 }
