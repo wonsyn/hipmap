96f0ea090e071cd04825bb031d0bc49259dc1cc0
wonsyn <sde3715@naver.com>
2022-10-31 17:40:24 +0900

    feat_wonsyn: S07P31B108-216 유저 정보 수정 Swagger 작성

--- Backend/hipmap/src/main/java/com/hipmap/domain/user/dto/request/UserEditRequest.java
+++ Backend/hipmap/src/main/java/com/hipmap/domain/user/dto/request/UserEditRequest.java
@@ -1,12 +1,18 @@
 package com.hipmap.domain.user.dto.request;
 
+import io.swagger.annotations.ApiModel;
+import io.swagger.annotations.ApiModelProperty;
 import lombok.Builder;
 import lombok.Data;
 
 @Data
 @Builder
+@ApiModel(value = "유저 정보 수정 Request 객체", description = "객체 내부의 nickname, label, followPrivate 값으로 수정해준다.")
 public class UserEditRequest {
+    @ApiModelProperty(value = "닉네임")
     String nickname;
+    @ApiModelProperty(value = "레이블 명")
     String label;
+    @ApiModelProperty(value = "팔로우 공개여부")
     boolean followPrivate;
 }
