df6df4510e4c684ce7df29a6545c4c4fd0280fc8
wonsyn <sde3715@naver.com>
2022-11-01 16:47:04 +0900

    refactor_wonsyn: S07P31B108-229 UserEntity, UserDetails 분리

--- Backend/hipmap/src/main/java/com/hipmap/domain/user/UserEntity.java
+++ Backend/hipmap/src/main/java/com/hipmap/domain/user/UserEntity.java
@@ -4,15 +4,8 @@ package com.hipmap.domain.user;
 import com.fasterxml.jackson.annotation.JsonIgnore;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.*;
-import org.springframework.security.core.GrantedAuthority;
-import org.springframework.security.core.authority.SimpleGrantedAuthority;
-import org.springframework.security.core.userdetails.UserDetails;
 
 import javax.persistence.*;
-import java.util.ArrayList;
-import java.util.Collection;
-import java.util.List;
-import java.util.stream.Collectors;
 
 
 @Entity
@@ -23,7 +16,7 @@ import java.util.stream.Collectors;
 @NoArgsConstructor
 @Builder
 @ToString
-public class UserEntity implements UserDetails {
+public class UserEntity {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -67,45 +60,4 @@ public class UserEntity implements UserDetails {
 
     @Enumerated(EnumType.STRING)
     private Admin role;
-
-    @ElementCollection(fetch = FetchType.EAGER)
-    @Builder.Default
-    private List<String> roles = new ArrayList<>();
-
-    @Override
-    public Collection<? extends GrantedAuthority> getAuthorities() {
-        return this.roles.stream()
-                .map(SimpleGrantedAuthority::new)
-                .collect(Collectors.toList());
-    }
-
-    @Override
-    public String getPassword() {
-        return null;
-    }
-
-    @Override
-    public String getUsername() {
-        return this.username;
-    }
-
-    @Override
-    public boolean isAccountNonExpired() {
-        return true;
-    }
-
-    @Override
-    public boolean isAccountNonLocked() {
-        return true;
-    }
-
-    @Override
-    public boolean isCredentialsNonExpired() {
-        return true;
-    }
-
-    @Override
-    public boolean isEnabled() {
-        return true;
-    }
 }
