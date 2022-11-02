b98fe97d7097c5ef64692a31dc221dee435973f8
wonsyn <sde3715@naver.com>
2022-11-02 09:13:29 +0900

    feat_wonsyn: S07P31B108-229 이메일 인증 시간 확인코드 작성

--- Backend/hipmap/src/main/java/com/hipmap/domain/user/AuthEmailService.java
+++ Backend/hipmap/src/main/java/com/hipmap/domain/user/AuthEmailService.java
@@ -2,12 +2,17 @@ package com.hipmap.domain.user;
 
 import com.amazonaws.services.kms.model.NotFoundException;
 import com.hipmap.domain.user.Exception.EmailAuthNotFoundException;
+import com.hipmap.domain.user.Exception.ExpiredEmailAuthException;
+import com.hipmap.domain.user.Exception.UserNotFoundException;
 import com.hipmap.global.util.RedisUtil;
 import lombok.RequiredArgsConstructor;
 import org.springframework.mail.SimpleMailMessage;
 import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
+import java.time.Duration;
+import java.time.LocalDateTime;
 import java.util.UUID;
 
 @RequiredArgsConstructor
@@ -29,14 +34,23 @@ public class AuthEmailService {
         String AUTH_LINK = "http://localhost:8080/api/user/auth/";
         if(email == null) throw new EmailAuthNotFoundException("멤버가 조회되지 않음");
         UUID uuid = UUID.randomUUID();
-        System.out.println("UUID String: " + uuid.toString());
         redisUtil.setDataExpire(uuid.toString(), email, 60 * 30L);
         sendMail(email, "회원가입 인증 메일입니다.", AUTH_LINK + uuid.toString());
     }
 
+    @Transactional
     public void authEmail(String key) throws NotFoundException {
         String memberEmail = redisUtil.getData(key);
         if(memberEmail == null) throw new EmailAuthNotFoundException("유효하지 않은 링크입니다.");
+
+        UserEntity user = userRepository.findByEmail(memberEmail).orElseThrow(UserNotFoundException::new);
+        LocalDateTime dateTime = user.getRegDateTime();
+        Duration duration = Duration.between(dateTime, LocalDateTime.now());
+        if(duration.getSeconds() >= 86400) {
+            throw new ExpiredEmailAuthException();
+        }
+
         redisUtil.deleteData(key);
+        user.auth();
     }
 }
