3ba0024c9459f3d1133aed043c833a5b17b9c043
wonsyn <sde3715@naver.com>
2022-11-02 09:14:38 +0900

    remove_wonsyn: S07P31B108-229 불필요 코드 제거

--- Backend/hipmap/src/main/java/com/hipmap/domain/user/UserRepository.java
+++ Backend/hipmap/src/main/java/com/hipmap/domain/user/UserRepository.java
@@ -9,6 +9,5 @@ public interface UserRepository extends JpaRepository<UserEntity, Long> {
     Optional<UserEntity> findByEmail(String email);
     Optional<UserEntity> findByUsernameAndPassword(String username, String password);
     Optional<UserEntity> findByUsername(String username);
-//    Optional<UserEntity> findByUserEmail(String email);
     List<UserEntity> findAllByUsername(String username);
 }
