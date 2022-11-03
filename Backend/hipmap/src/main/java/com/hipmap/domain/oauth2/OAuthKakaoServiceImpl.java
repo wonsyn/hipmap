786934b440dd603c55b1814c46b449b628993c00
wonsyn <sde3715@naver.com>
2022-11-03 09:02:54 +0900

    feat_wonsyn: S07P31B108-230 카카오 로그인 요청 변수처리

--- Backend/hipmap/src/main/java/com/hipmap/domain/oauth2/OAuthKakaoServiceImpl.java
+++ Backend/hipmap/src/main/java/com/hipmap/domain/oauth2/OAuthKakaoServiceImpl.java
@@ -14,6 +14,16 @@ import java.util.Map;
 @Service
 public class OAuthKakaoServiceImpl implements OAuthKakaoService{
 
+    @Value("${spring.security.oauth2.client.registration.kakao.client-secret}")
+    String clientSecret;
+    @Value("${spring.security.oauth2.client.registration.kakao.client-id}")
+    String clientId;
+    @Value("${spring.security.oauth2.client.provider.kakao.token-uri}")
+    String tokenUri;
+    @Value("${spring.security.oauth2.client.provider.kakao.user-info-uri}")
+    String userInfoUri;
+
+
     @Override
     public Map<String, String> auth(String code) {
         // POST방식으로 key=value 데이터 요청(카카오쪽으로 찌른다)
@@ -25,10 +35,10 @@ public class OAuthKakaoServiceImpl implements OAuthKakaoService{
         // 일단 param들 변수화 안함.. 추후 예정
         MultiValueMap<String, String> params = new LinkedMultiValueMap<>();
         params.add("grant_type", "authorization_code");
-        params.add("client_id", "29250d362049cababb2b673f785f8ff2");
-        params.add("redirect_uri", "http://localhost:8080/api/oauth/kakao");
+        params.add("client_id", clientId);
+        params.add("redirect_uri", "http://localhost:8080/api/oauth/kakao/result");
         params.add("code", code);
-        params.add("client_secret", "NXpC0rZwukqYSUQTJK4368oNotY5rFJb");
+        params.add("client_secret", clientSecret);
 
         // HttpHeader와 HttpBody를 하나의 오브젝트로 담는다
         HttpEntity<MultiValueMap<String,String>> kakaoTokenRequest =
@@ -36,7 +46,7 @@ public class OAuthKakaoServiceImpl implements OAuthKakaoService{
 
         // 실제요청
         ResponseEntity<String> response = restTemplate.exchange(
-                "https://kauth.kakao.com/oauth/token",
+                tokenUri,
                 HttpMethod.POST,
                 kakaoTokenRequest,
                 String.class
