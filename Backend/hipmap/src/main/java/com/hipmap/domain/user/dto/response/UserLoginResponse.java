e7bac813c551c3f522e40d2951b937ce7670976a
wonsyn <sde3715@naver.com>
2022-11-03 13:55:51 +0900

    refactor_wonsyn: 토큰 처리 Map -> Tokens 객체 변경

--- Backend/hipmap/src/main/java/com/hipmap/domain/user/dto/response/UserLoginResponse.java
+++ Backend/hipmap/src/main/java/com/hipmap/domain/user/dto/response/UserLoginResponse.java
@@ -1,18 +1,15 @@
 package com.hipmap.domain.user.dto.response;
 
+import com.hipmap.domain.user.dto.Tokens;
 import io.swagger.annotations.ApiModel;
 import io.swagger.annotations.ApiModelProperty;
 import lombok.Builder;
 import lombok.Data;
-
-import java.util.Map;
 
 @Data
 @Builder
 @ApiModel(value = "로그인 결과 토큰 리스트", description = "로그인 결과로 생성된 JWT토큰과 만료일자가 담긴 객체")
 public class UserLoginResponse {
     @ApiModelProperty(value = "JWT 토큰, (accessToken, refreshToken)")
-    Map<String, String> tokens;
-    @ApiModelProperty(value = "만료 일자")
-    Long expireMilliSec;
+    Tokens tokens;
 }
