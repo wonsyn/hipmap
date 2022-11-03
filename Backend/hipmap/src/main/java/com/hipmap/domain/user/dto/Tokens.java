ccbd6168f574080ec10bf297fef311def4afda64
wonsyn <sde3715@naver.com>
2022-11-03 13:54:43 +0900

    feat_wonsyn: Tokens Swagger 작성

--- Backend/hipmap/src/main/java/com/hipmap/domain/user/dto/Tokens.java
+++ Backend/hipmap/src/main/java/com/hipmap/domain/user/dto/Tokens.java
@@ -1,12 +1,18 @@
 package com.hipmap.domain.user.dto;
 
+import io.swagger.annotations.ApiModel;
+import io.swagger.annotations.ApiModelProperty;
 import lombok.Builder;
 import lombok.Data;
 
 @Data
 @Builder
+@ApiModel(value = "토큰을 담아주는 객체")
 public class Tokens {
+    @ApiModelProperty(value = "access-token")
     String accessToken;
+    @ApiModelProperty(value = "refresh-token")
     String refreshToken;
+    @ApiModelProperty(value = "token 만료 시간 (milli sec)")
     long expireMilliSec;
 }
