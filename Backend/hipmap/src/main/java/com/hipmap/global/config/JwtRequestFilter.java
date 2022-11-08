f5b9d59bf9f386d8e636ffa640ed4b7422e717ff
wonsyn <sde3715@naver.com>
2022-11-08 13:34:51 +0900

    feat_wonsyn: 07P31B108-162 JWT필터 예외처리 구현

--- Backend/hipmap/src/main/java/com/hipmap/global/config/JwtRequestFilter.java
+++ Backend/hipmap/src/main/java/com/hipmap/global/config/JwtRequestFilter.java
@@ -3,6 +3,7 @@ package com.hipmap.global.config;
 import com.hipmap.domain.user.UserService;
 import com.hipmap.global.util.JwtUtil;
 import io.jsonwebtoken.ExpiredJwtException;
+import io.jsonwebtoken.JwtException;
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.context.SecurityContextHolder;
@@ -31,33 +32,22 @@ public class JwtRequestFilter extends OncePerRequestFilter {
             if(jwtToken != null){
                 jwt = jwtToken;
                 username = jwtUtil.getUsername(jwt);
-            } else {
-                httpServletResponse.setStatus(401);
-                filterChain.doFilter(httpServletRequest,httpServletResponse);
-//                throw new AuthenticationException("cannot find JWT token ");
             }
             if(username!=null) {
                 UserDetails userDetails = userDetailsService.loadUserByUsername(username);
 
-                try {
-                    if (jwtUtil.validateToken(jwt, userDetails)) {
-                        UsernamePasswordAuthenticationToken usernamePasswordAuthenticationToken = new UsernamePasswordAuthenticationToken(userDetails, null, userDetails.getAuthorities());
-                        usernamePasswordAuthenticationToken.setDetails(new WebAuthenticationDetailsSource().buildDetails(httpServletRequest));
-                        SecurityContextHolder.getContext().setAuthentication(usernamePasswordAuthenticationToken);
-                    }
-                } catch (IllegalArgumentException e) {
-                    System.out.println("예외 발생 JWT null");
-                    filterChain.doFilter(httpServletRequest,httpServletResponse);
+                if (jwtUtil.validateToken(jwt, userDetails)) {
+                    UsernamePasswordAuthenticationToken usernamePasswordAuthenticationToken = new UsernamePasswordAuthenticationToken(userDetails, null, userDetails.getAuthorities());
+                    usernamePasswordAuthenticationToken.setDetails(new WebAuthenticationDetailsSource().buildDetails(httpServletRequest));
+                    SecurityContextHolder.getContext().setAuthentication(usernamePasswordAuthenticationToken);
                 }
-            } else {
-                httpServletResponse.setStatus(401);
-                filterChain.doFilter(httpServletRequest,httpServletResponse);
-//                throw new IllegalTokenException("올바르지 않은 토큰");
             }
         } catch (ExpiredJwtException e){
-            httpServletResponse.setStatus(401);
-            filterChain.doFilter(httpServletRequest,httpServletResponse);
-//            throw new AuthenticationException("expired access token; call /api/jwt/re-issue before you call other API");
+            e.printStackTrace();
+            httpServletRequest.setAttribute("exception", "Expired");
+        } catch (JwtException e){
+            e.printStackTrace();
+            httpServletRequest.setAttribute("exception", "Invalid");
         }
 
         filterChain.doFilter(httpServletRequest,httpServletResponse);
