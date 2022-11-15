b4dfb3a64325bd0b18639cb15cec306b095806a4
hsh102103 <hsh102103@naver.com>
2022-11-15 15:57:58 +0900

    fix_hwang: area Swagger 미출력 오류 수정

--- Backend/hipmap/src/main/java/com/hipmap/domain/area/dto/response/GetSidoListResponse.java
+++ Backend/hipmap/src/main/java/com/hipmap/domain/area/dto/response/GetSidoListResponse.java
@@ -9,9 +9,9 @@ import java.util.ArrayList;
 import java.util.List;
 
 @Data
-@ApiModel(value = "시/도 요청 응답 객체", description = "시/도 조회시 나온 결과 리스트를 리턴하는 객체")
+@ApiModel(value = "시도 요청 응답 객체", description = "시/도 조회시 나온 결과 리스트를 리턴하는 객체")
 public class GetSidoListResponse {
-    @ApiModelProperty(value = "시/도 리스트")
+    @ApiModelProperty(value = "시도 리스트")
     List<GetSidoResponse> sidoList;
 
     public GetSidoListResponse(List<SidoEntity> list) {
